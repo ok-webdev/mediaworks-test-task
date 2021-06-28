9cd10af4c28625ad7c7baf508c242dec2ffff096
ok-webdev <oleg.kokoshinskii@gmail.com>
2021-06-28 20:39:53 +1000

    avoid overflow under modal

--- js/script.js
+++ js/script.js
@@ -4,25 +4,28 @@ const closeModal = document.querySelector('.modal__close');
 const modalForm = document.querySelector('.modal__form');
 const account = document.querySelector('#account');
 const inputs = document.querySelectorAll('input');
+const body = document.querySelector('body');
 
 function toggleBackdrop() {
   backdrop.classList.toggle('visible');
 }
 
 function hideModal() {
-  modal.classList.remove('visible')
+  modal.classList.remove('visible');
   toggleBackdrop();
-  inputs.forEach(input => input.value = '');
+  inputs.forEach((input) => (input.value = ''));
+  body.style.overflow = 'auto';
 }
 
-function showModal () {
+function showModal() {
   modal.classList.add('visible');
   toggleBackdrop();
+  body.style.overflow = 'hidden';
 }
 
 account.addEventListener('click', showModal);
 closeModal.addEventListener('click', hideModal);
-modalForm.addEventListener('submit', event => {
+modalForm.addEventListener('submit', (event) => {
   event.preventDefault();
   hideModal();
 });
